17fd963f78209b6a747d72710549a503735c2620
kushal1997 <kushalrao103@gmail.com>
2024-06-04 18:04:22 +0530

    home page header ui part complete

--- App/Screens/HomeScreen/Home.jsx
+++ App/Screens/HomeScreen/Home.jsx
@@ -1,10 +1,11 @@
 import { StyleSheet, Text, View } from 'react-native'
 import React from 'react'
+import Header from './Header'
 
 const Home = () => {
   return (
     <View>
-      <Text>Home</Text>
+      <Header/>
     </View>
   )
 }
